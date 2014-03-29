b07aea062c3215229bcc9f8592cc0ec50f6d88b6
kimjoohwee <kjhwee91@gmail.com>
2014-03-29 10:53:02 +0900

    133 db

---
+++ 133_code/DBbasic/database.java
@@ -0,0 +1,30 @@
+class database {
+
+	public static void main(String[] args){
+		reculsive(9);
+	}
+
+	private static void reculsive(long i) {
+
+		long n = 0;
+		long n_1 = 0;
+		long n_2 = 0;
+
+		for (int j=1; j<=i ; j++){
+			if (j==1){
+				n_2 = 2;
+			}
+			else if(j == 2){
+				n_1 = 3;
+			}
+			else {
+				n = n_1 * n_2;
+				n_2 = n_1;
+				n_1 = n;
+			}
+		}
+
+		System.out.println(n);
+	}
+
+}
---
+++ 133_code/DBbasic/database2.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class database2 {
+
+	public static void main(String[] args){
+		String a = "apple";
+		String temp[] = a.split("");
+
+		Arrays.sort(temp);
+		System.out.println(Arrays.toString(temp));
+	}
+
+}
